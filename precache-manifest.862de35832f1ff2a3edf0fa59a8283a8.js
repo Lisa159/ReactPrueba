22a836f1e3c5f1bf3805212e1476c8eee3091264
Aida María González <UO245925@uniovi.es>
2019-02-28 18:24:29 +0100

    Updates

---
+++ precache-manifest.862de35832f1ff2a3edf0fa59a8283a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ReactPrueba/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ba595fbee55e7effaf08",
+    "url": "/ReactPrueba/static/js/runtime~main.ba595fbe.js"
+  },
+  {
+    "revision": "db837b73477867be0d3e",
+    "url": "/ReactPrueba/static/js/main.db837b73.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/ReactPrueba/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "db837b73477867be0d3e",
+    "url": "/ReactPrueba/static/css/main.65cdfdc6.chunk.css"
+  },
+  {
+    "revision": "d7ff57bdc16e5b268cf839d45e0d66c5",
+    "url": "/ReactPrueba/index.html"
+  }
+];
