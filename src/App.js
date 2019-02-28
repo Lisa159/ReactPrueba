de02fd992bd00d5753ca00e05e47ee23d70891ff
Aida María González <UO245925@uniovi.es>
2019-02-28 21:00:32 +0100

    Added files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from "react";
+import { ToastProvider } from "react-toast-notifications";
+import { ToasterNotification } from "@util-components";
 
+import Routes from "./routes";
+import { ThemeProvider } from "styled-components";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+
+import theme from "./utils/theme";
+import "normalize.css";
+import "./index.css";
+import "@inrupt/inrupt-atomic-style-guide";
+
+library.add(fas);
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <ToastProvider
+        components={{ Toast: ToasterNotification }}
+        placement="top-center"
+        autoDismiss={true}
+        autoDismissTimeout={3000}
+      >
+        <ThemeProvider theme={theme}>
+          <Fragment>
+            <Routes />
+          </Fragment>
+        </ThemeProvider>
+      </ToastProvider>
     );
   }
 }
